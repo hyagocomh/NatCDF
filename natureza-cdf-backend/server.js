ac6e553252acf95bbb792cef851fe4b0e1fb0412
hyagocomh <hya.az.le@gmail.com>
2026-01-15 17:15:03 -0300

    rollback3

--- natureza-cdf-backend/server.js
+++ natureza-cdf-backend/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
 const nodemailer = require('nodemailer');
 const { MercadoPagoConfig, Preference } = require('mercadopago');
 
